6c9b91e3a529ae7c7d5f0acd78767cc354ee6a01
PavelZinchenko <zipas.games@gmail.com>
2023-06-28 16:58:10 +0400

    update

---
+++ Assets/Scripts/TriggerDetector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerDetector : MonoBehaviour
+{
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private UnityEvent _triggerEnter = new();
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!_layerMask.Contains(collider.gameObject.layer)) return;
+        _triggerEnter.Invoke();
+    }
+}
