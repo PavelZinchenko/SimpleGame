ae376b92d46bde31a8d609d9cf8ce2aff02ac021
PavelZinchenko <zipas.games@gmail.com>
2023-06-30 18:01:54 +0400

    game state machine

---
+++ Assets/Scripts/Camera/FixedSpeedCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Camera
+{
+    public class FixedSpeedCamera : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 2;
+
+        private void Update()
+        {
+            var position = transform.localPosition;
+            position.x += Time.deltaTime * _speed;
+            transform.localPosition = position;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Camera
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 2;
+
+        private readonly List<Transform> _targets = new();
+
+        public void Initialize(params Transform[] players)
+        {
+            _targets.Clear();
+            _targets.AddRange(players);
+        }
+
+        private void Update()
+        {
+            float targetX = 0;
+            int targetCount = 0;
+
+            foreach (var item in _targets)
+            {
+                if (!item) continue;
+                targetCount++;
+                targetX += item.position.x;
+            }
+
+            if (targetCount == 0) return;
+
+            targetX /= targetCount;
+            var position = transform.localPosition;
+
+            if (position.x > targetX) return;
+
+            position.x = Mathf.Lerp(position.x, targetX, Time.deltaTime * _speed);
+            transform.localPosition = position;
+        }
+    }
+}
